eb15a7dfb6fb5572bb7dbbc682ce225b2ea8a97d
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-11-05 17:21:21 -0800

    Update get_earliest after seeing official solution
    So much clearer!

--- earliest/earliest.py
+++ earliest/earliest.py
@@ -1,12 +1,7 @@
 def get_earliest(*dates):
-    earliest = dates[0]
-    for date in dates:
-        if to_ymd(date) < to_ymd(earliest):
-            earliest = date
-
-    return earliest
+    return min(dates, key=date_key)
 
 
-def to_ymd(date):
-    """Assume input is in format MM/DD/YYYY. Return formatted as YYYYMMDD."""
-    return date[-4:] + date[:2] + date[3:5]
+def date_key(date):
+    (m, d, y) = date.split('/')
+    return (y, m, d)
