f978f990bce8fb466b1974d1e37906dfc092b81d
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-01-28 14:32:38 -0800

    Update meetup_date to use Calendar
    Not the most efficient solution, but I think it is still more readable than others

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -1,25 +1,10 @@
+from calendar import Calendar
 from collections import defaultdict
-from datetime import date, timedelta
-from enum import Enum
+from datetime import date
+from enum import IntEnum
 
 
-def meetup_date(year, month, nth=4, weekday=3):
-    """Return the date when a monthly meetup should take place."""
-    dates_by_weekday = defaultdict(list)
-    day = date(year, month, 1)
-    while True:  # Easier to build the whole calendar month due to edge cases
-        dates_by_weekday[day.weekday()].append(day)
-        day = day + timedelta(days=1)
-        if day.month != month:
-            break
-
-    if 0 < nth:
-        nth -= 1
-
-    return dates_by_weekday[weekday][nth]
-
-
-class Weekday:
+class Weekday(IntEnum):
     MONDAY = 0
     TUESDAY = 1
     WEDNESDAY = 2
@@ -27,3 +12,24 @@ class Weekday:
     FRIDAY = 4
     SATURDAY = 5
     SUNDAY = 6
+
+
+def meetup_date(year, month, nth=4, weekday=Weekday.THURSDAY):
+    """Return the date when a monthly meetup should take place."""
+    dates_by_weekday = _get_dates_by_weekday(year, month)
+    if 0 < nth:
+        nth -= 1
+
+    return dates_by_weekday[weekday][nth]
+
+
+def _get_dates_by_weekday(year, month):
+    """Return a dict with weekdays as keys and sorted days in the month as values."""
+    cal = Calendar()
+    dates_by_weekday = defaultdict(list)
+    for day_of_month, weekday in cal.itermonthdays2(year, month):
+        if day_of_month == 0:
+            continue
+        dates_by_weekday[weekday].append(date(year, month, day_of_month))
+
+    return dates_by_weekday
