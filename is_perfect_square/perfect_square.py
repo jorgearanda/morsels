4a1d4e08396789a7b9634d0eb0cd2b5ca0d3132a
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-01-30 10:12:55 -0800

    Update perfect_square after solutions email

--- is_perfect_square/perfect_square.py
+++ is_perfect_square/perfect_square.py
@@ -1,18 +1,19 @@
 import cmath
-from decimal import Decimal, getcontext
+from decimal import Context, Decimal, getcontext, localcontext
 
 
 def is_perfect_square(num, *, complex=False):
     if complex:
         root = cmath.sqrt(num)
-        return _rounds_to_int(root.real) and _rounds_to_int(root.imag)
+        return _is_integer(root.real) and _is_integer(root.imag)
     else:
         if num < 0:
             return False
-        getcontext().prec = 100
-        root = Decimal(num).sqrt()
-        return _rounds_to_int(root)
+        digit_count = len(str(num))
+        with localcontext(Context(prec=digit_count * 2)):
+            root = Decimal(num).sqrt()
+        return _is_integer(root)
 
 
-def _rounds_to_int(num):
+def _is_integer(num):
     return int(num) == num
