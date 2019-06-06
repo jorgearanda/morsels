1f3a5d92bf48ac10b8d481e3572727863bac25ac
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-06-06 09:56:54 -0700

    Improve on all_same solution

--- all_same/all_same.py
+++ all_same/all_same.py
@@ -1,22 +1,6 @@
-from collections.abc import Hashable
-
-
 def all_same(seq):
-    first_item = uninitialized = object()
-    for item in seq:
-        hashed_item = _get_hashed(item)
-        if first_item == uninitialized:
-            first_item = hashed_item
-        if first_item != hashed_item:
-            return False
-
-    return True
-
-
-def _get_hashed(item):
-    if isinstance(item, Hashable):
-        return hash(item)
-    elif isinstance(item, list):
-        return hash(tuple(item))
-    else:
-        return hash(tuple(sorted(item.items())))
+    first_item = next(iter(seq), None)
+    return all(
+        item == first_item
+        for item in seq
+    )
